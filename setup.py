1f38747391f41d41f7b46805d9f13a2d7ae1b91c
Vladimir Vyalov <vyalovva@gdeetotdom.ru>
2012-11-25 16:31:54 +0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='yasv',
+    version='0.1',
+    packages=find_packages(),
+    author='Vladimir Vyalov',
+    author_email='vyalov.v@gmail.com',
+    description=('Yet Another Simple Validator'),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha'
+    ],
+)
