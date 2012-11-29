b238fb89a8ea3f24b4d698e2587175ada33e8978
Vladimir Vyalov <vyalovva@gdeetotdom.ru>
2012-11-29 23:34:05 +0400

    fix tests

--- yasv/tests.py
+++ yasv/tests.py
@@ -1,4 +1,5 @@
 import unittest
+from collections import namedtuple
 
 from yasv import *
 
@@ -8,70 +9,52 @@ class TestSchema(unittest.TestCase):
     def test_schema(self):
         is_in = IsIn()
         required = Required()
-        s = Schema({
-            'foo': Field('Foo', required),
-            'bar': Field('Bar', is_in([1, 2])),
-        })
-        d = s.validate({
-            'foo': 1,
-            'bar': 2
-        })
-        self.assertEqual(d, {'foo': 1, 'bar': 2})
 
-        try:
-            s.validate({
-                'foo': 1,
-                'bar': 3
-            })
-        except ValidationError, e:
-            response = {
-                'foo': {
-                    'errors': [],
-                    'value': 1,
-                    'label': 'Foo'
-                },
-                'bar': {
-                    'errors': ['Value not in presets: (1, 2).'],
-                    'value': 3,
-                    'label': 'Bar',
-                },
-            }
-            self.assertEqual(e.error_response, response)
-        else:
-            self.fail('Exception was not raised for invalid data')
+        class TestSchema(Schema):
+            foo = Field('Foo', required)
+            bar = Field('Bar', is_in([1, 2]))
+
+        s = TestSchema({'foo': 1, 'bar': 2})
+        self.assertEqual(s.is_valid(), True)
+
+        Data = namedtuple('Data', ['foo', 'bar'])
+        s = TestSchema(Data(foo=1, bar=2))
+        self.assertEqual(s.is_valid(), True)
+
+        s = TestSchema({'foo': 1, 'bar': 3})
+        self.assertEqual(s.is_valid(), False)
+        self.assertEqual(s._unbound_fields['bar'].errors,
+            ['Value not in presets: (1, 2).'])
 
     def test_is_url(self):
-        s = Schema({'url': Field('URL', IsURL())})
+        class TestSchema(Schema):
+            url = Field('URL', IsURL())
 
-        valid_data = {'url': 'http://example.com'}
-        d = s.validate(valid_data)
-        self.assertEqual(d, valid_data)
+        s = TestSchema({'url': 'http://example.com'})
+        self.assertEqual(s.is_valid(), True)
 
-        valid_data = {'url': None}
-        d = s.validate(valid_data)
-        self.assertEqual(d, valid_data)
+        s = TestSchema({'url': None})
+        self.assertEqual(s.is_valid(), True)
 
-        invalid_data = {'url': 'www.example.com'}
-        with self.assertRaises(ValidationError):
-            s.validate(invalid_data)
+        s = TestSchema({'url': 'www.example.com'})
+        self.assertEqual(s.is_valid(), False)
 
     def test_required(self):
-        s = Schema({'foo': Field('Foo', Required())})
+        class TestSchema(Schema):
+            foo = Field('Foo', Required())
 
-        invalid_data = {'foo': ' '}
-        with self.assertRaises(ValidationError):
-            s.validate(invalid_data)
+        s = TestSchema({'foo': ' '})
+        self.assertEqual(s.is_valid(), False)
 
     def test_min_len(self):
-        s = Schema({'foo': Field('Foo', MinLen()(2))})
+        class TestSchema(Schema):
+            foo = Field('Foo', MinLen()(2))
 
-        valid_data = {'foo': '12'}
-        d = s.validate(valid_data)
-        self.assertEqual(d, valid_data)
+        s = TestSchema({'foo': '12'})
+        self.assertEqual(s.is_valid(), True)
 
-        invalid_data = {'foo': ' '}
-        with self.assertRaises(ValidationError):
-            s.validate(invalid_data)
+        s = TestSchema({'foo': ' '})
+        self.assertEqual(s.is_valid(), False)
 
 
 if __name__ == '__main__':
