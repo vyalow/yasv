db5c23110c6b076c801f6ebd62094a49a8460e57
Vladimir Vyalov <vyalov.v@gmail.com>
2013-06-25 10:30:09 +0400

    Bump version

--- yasv/__init__.py
+++ yasv/__init__.py
@@ -1,7 +1,7 @@
 from .core import Field, Schema
 from .validators import *
 
-VERSION = (0, 1, 8, 'dev')
+VERSION = (0, 1, 9, 'dev')
 __version__ = '.'.join(map(str, VERSION[0:3])) + ''.join(VERSION[3:])
 
 # -eof meta-
