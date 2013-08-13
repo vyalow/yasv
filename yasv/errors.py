36bc77d3ff0ab598183aee496a4d2c8059b571c2
Vladimir Vyalov <vyalov.v@gmail.com>
2013-08-13 14:14:28 +0400

    Add errors.py

---
+++ yasv/errors.py
@@ -0,0 +1,13 @@
+class InvalidCleanedDataError(Exception):
+    def __init__(self, message='', field_name='', field_label=''):
+        self.message = message
+        self.field_name = field_name
+        self.field_label = field_label
+
+
+class ValidationError(Exception):
+    """ Raised when a validator fails to validate its input.
+    """
+    def __init__(self, message=''):
+        self.error_response = {}
+        self.message = message
