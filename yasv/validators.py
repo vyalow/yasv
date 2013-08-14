7929f36dd8e32f9dcd3536cbeda92e9e92b58b23
Vladimir Vyalov <vyalov.v@gmail.com>
2013-08-14 14:35:26 +0400

    Fix template key.

--- yasv/validators.py
+++ yasv/validators.py
@@ -65,7 +65,7 @@ class String(Validator):
     """ Base class for validators. Check that the data is instance of
     str or unicode.
     """
-    templates = {'wrong type': 'Illegal type. String expected: {0}.'}
+    templates = {'wrong_type': 'Illegal type. String expected: {0}.'}
 
     def specified_type(self):
         if isinstance(self.value, string_types):
